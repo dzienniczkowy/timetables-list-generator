3078d1e5fed4b5352d04dd5fac47cab34af308a6
Mikołaj Pich <m.pich@outlook.com>
2017-11-04 04:26:34 +0100

    cs fix

--- src/TimetableChecker.php
+++ src/TimetableChecker.php
@@ -2,7 +2,6 @@
 
 namespace Wulkanowy\TimetablesListGenerator;
 
-
 use Colors\Color;
 use Exception;
 use GuzzleHttp\Client;
@@ -15,112 +14,112 @@ use Psr\Http\Message\ResponseInterface;
 
 class TimetableChecker
 {
-	/**
-	 * @var Client
-	 */
-	private $client;
+    /**
+     * @var Client
+     */
+    private $client;
 
-	/**
-	 * @var array
-	 */
-	private $schools;
+    /**
+     * @var array
+     */
+    private $schools;
 
-	/**
-	 * @var int
-	 */
-	private $numberOfAll;
+    /**
+     * @var int
+     */
+    private $numberOfAll;
 
-	/**
-	 * @var int
-	 */
-	private $processed = 0;
+    /**
+     * @var int
+     */
+    private $processed = 0;
 
-	/**
-	 * @var int
-	 */
-	private $connectError = 0;
+    /**
+     * @var int
+     */
+    private $connectError = 0;
 
-	/**
-	 * @var int
-	 */
-	private $clientError = 0;
+    /**
+     * @var int
+     */
+    private $clientError = 0;
 
-	/**
-	 * @var int
-	 */
-	private $error = 0;
+    /**
+     * @var int
+     */
+    private $error = 0;
 
-	public function __construct(Client $client, array $schools)
-	{
-		$this->client = $client;
-		$this->schools = $schools;
-		$this->numberOfAll = count($schools);
-		sort($this->schools);
-	}
+    public function __construct(Client $client, array $schools)
+    {
+        $this->client = $client;
+        $this->schools = $schools;
+        $this->numberOfAll = count($schools);
+        sort($this->schools);
+    }
 
-	public function check() : array
-	{
-		echo PHP_EOL;
-		$c = new Color();
-		$filtered = [];
-		$requests = function (array $items) {
-			foreach ($items as $key => $value) {
-				yield new Request('GET', $value['timetables'][0]);
-			}
-		};
-		$schools = $this->schools;
-		$pool = new Pool($this->client, $requests($this->schools), [
-			'concurrency' => 50,
-			'fulfilled'   => function (ResponseInterface $response, $index) use ($schools, &$filtered, $c) {
-				$value = $schools[$index];
-				echo '['.$this->processed.'/'.$this->numberOfAll.'] '.$value['www'].' – ';
-				$str = $response->getBody()->getContents();
-				if (strpos($str, 'Plan lekcji Optivum firmy VULCAN') !== false) {
-					echo $c('Szkoła używa Planu lekcji Optivum.')->fg('green');
-					$value['url'] = $value['timetables'][0];
-					$filtered[$index] = $value;
-				} else {
-					echo $c('nie wiem.')->fg('dark_gray');
-				}
+    public function check() : array
+    {
+        echo PHP_EOL;
+        $c = new Color();
+        $filtered = [];
+        $requests = function (array $items) {
+            foreach ($items as $key => $value) {
+                yield new Request('GET', $value['timetables'][0]);
+            }
+        };
+        $schools = $this->schools;
+        $pool = new Pool($this->client, $requests($this->schools), [
+            'concurrency' => 50,
+            'fulfilled'   => function (ResponseInterface $response, $index) use ($schools, &$filtered, $c) {
+                $value = $schools[$index];
+                echo '['.$this->processed.'/'.$this->numberOfAll.'] '.$value['www'].' – ';
+                $str = $response->getBody()->getContents();
+                if (strpos($str, 'Plan lekcji Optivum firmy VULCAN') !== false) {
+                    echo $c('Szkoła używa Planu lekcji Optivum.')->fg('green');
+                    $value['url'] = $value['timetables'][0];
+                    $filtered[$index] = $value;
+                } else {
+                    echo $c('nie wiem.')->fg('dark_gray');
+                }
 
-				$this->processed++;
-				echo PHP_EOL;
-			},
-			'rejected' => function (RequestException $e, $index) use ($schools, $c) {
-				$value = $schools[$index];
-				$path = $value['www'];
-				echo '['.$this->processed.'/'.$this->numberOfAll.'] '.$path.' – ';
+                $this->processed++;
+                echo PHP_EOL;
+            },
+            'rejected' => function (RequestException $e, $index) use ($schools, $c) {
+                $value = $schools[$index];
+                $path = $value['www'];
+                echo '['.$this->processed.'/'.$this->numberOfAll.'] '.$path.' – ';
 
-				$this->error++;
-				$this->processed++;
-				if ($e instanceof ConnectException) {
-					$this->connectError++;
-					echo $c('Nie nazwiązano połączenia')->fg('red');
-				} elseif ($e instanceof ClientException) {
-					$this->clientError++;
-					echo $c('Błąd połączenia')->fg('red');
-				} else {
-					echo $c('Inny błąd')->fg('yellow');
-				}
-				echo PHP_EOL;
-			},
-		]);
+                $this->error++;
+                $this->processed++;
+                if ($e instanceof ConnectException) {
+                    $this->connectError++;
+                    echo $c('Nie nazwiązano połączenia')->fg('red');
+                } elseif ($e instanceof ClientException) {
+                    $this->clientError++;
+                    echo $c('Błąd połączenia')->fg('red');
+                } else {
+                    echo $c('Inny błąd')->fg('yellow');
+                }
+                echo PHP_EOL;
+            },
+        ]);
 
-		try {
-			$pool->promise()->wait();
-		} catch (Exception $e) {
-			echo $e->getMessage().PHP_EOL;
-		}
-		usort($filtered, function ($a, $b) {
-			return $a['url'] <=> $b['url'];
-		});
+        try {
+            $pool->promise()->wait();
+        } catch (Exception $e) {
+            echo $e->getMessage().PHP_EOL;
+        }
+        usort($filtered, function ($a, $b) {
+            return $a['url'] <=> $b['url'];
+        });
 
-		return [
-			'filtered'      => $filtered,
-			'errors'        => $this->error,
-			'clientErrors'  => $this->clientError,
-			'connectErrors' => $this->connectError,
-			'total'         => $this->numberOfAll,
-		];
-	}
+        return [
+            'filtered'      => $filtered,
+            'errors'        => $this->error,
+            'clientErrors'  => $this->clientError,
+            'connectErrors' => $this->connectError,
+            'total'         => $this->numberOfAll,
+        ];
+    }
 }
